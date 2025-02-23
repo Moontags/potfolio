1201e5da539fa996817090864cc0a7e6a344a8b0
Jari Peltola <jena9988@gmail.com>
2025-02-23 19:08:21 +0200

    Smoother color theme

--- app/page.tsx
+++ app/page.tsx
@@ -6,24 +6,25 @@ import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
 
 export default function Home() {
-  const { theme, setTheme } = useTheme();
+  useTheme(); 
   const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
     setMounted(true);
   }, []);
 
-  // Renderöidään käyttöliittymä heti, vaikka tema ei vielä olisi täysin asetettu.
+  if (!mounted) return <div className="min-h-screen bg-black"></div>;
+
   return (
     <div className={`relative min-h-screen flex flex-col items-center justify-center transition-colors duration-300 bg-black text-white overflow-hidden`}>
       
       <div className="absolute inset-0 stars"></div>
       
       <motion.h1
-        initial={{ scale: 0, opacity: 0 }}
+        initial={{ scale: 0, opacity: 0  }}
         animate={{ scale: 1, opacity: 1 }}
         transition={{ duration: 1, ease: "easeOut" }}
-        className="absolute top-16 text-6xl font-bold text-white"
+        className="absolute top-16 text-6xl font-bold text-gray-200"
       >
         Welcome! 
       </motion.h1>
@@ -31,7 +32,7 @@ export default function Home() {
       <motion.div
         initial={{ scale: 0, opacity: 0, rotate: 0 }}
         animate={{ scale: 2.2, opacity: 1, rotate: 720 }}
-        transition={{ repeat: Infinity, repeatType: "reverse", duration: 2, ease: "linear" }}
+        transition={{ repeat: Infinity, repeatType: "reverse", duration: 2.5, ease: "linear" }}
         className="relative z-10"
       >
         <Image
@@ -44,10 +45,10 @@ export default function Home() {
       </motion.div>
 
       <motion.p
-        initial={{ scale: 0, opacity: 0 }}
+        initial={{ scale: 0, opacity: 0  }}
         animate={{ scale: 1, opacity: 1 }}
         transition={{ duration: 1, ease: "easeOut" }}
-        className="text-2xl max-w-2xl text-center mt-12"
+        className="text-2xl text-gray-200 max-w-2xl text-center mt-12"
       >
          Explore my portfolio and my journey as a developer
       </motion.p>
@@ -58,13 +59,14 @@ export default function Home() {
         animate={{ scale: 1, opacity: 1, rotate: 360 }}
         transition={{ duration: 0.5 }}
         className="mt-12 px-6 py-2 text-lg font-medium flex items-center justify-center 
-                  bg-gradient-to-b from-blue-400 via-blue-500 to-blue-400 
-                  text-white rounded-full shadow-lg 
+                  bg-gradient-to-b from-green-400 via-green-500 to-green-400 
+                  text-gray-100 rounded-full shadow-lg 
                   transition-all duration-300 ease-in-out 
                   hover:shadow-xl hover:brightness-110 active:shadow-md"
       >
         Start Here
       </motion.a>
+
     </div>
   );
 }
